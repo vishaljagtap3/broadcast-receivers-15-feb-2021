338da66ffd7c6e549b47884e1e076a8024d75361
vishaljagtap3 <vishal.jagtap@bitcode.in>
2021-02-16 14:24:29 +0530

    Broadcast Demo

--- app/src/main/java/in/bitcode/broadcastdemo1/MainActivity.java
+++ app/src/main/java/in/bitcode/broadcastdemo1/MainActivity.java
@@ -3,18 +3,39 @@ package in.bitcode.broadcastdemo1;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.BroadcastReceiver;
+import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     private BroadcastReceiver mBrDemo;
+    private Button mBtnRegisterBR;
+
+    private BroadcastReceiver mBREvent = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            Toast.makeText( context, "Sticky: " + intent.getAction(), Toast.LENGTH_LONG).show();
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mBtnRegisterBR = findViewById(R.id.btnRegisterBR);
+        mBtnRegisterBR.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                IntentFilter intentFilter = new IntentFilter("in.bitcode.some.EVENT");
+                registerReceiver(mBREvent, intentFilter);
+            }
+        });
 
         mBrDemo = new DemoBR();
 
